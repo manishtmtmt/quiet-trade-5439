5d7719786be015c20ed5fcf5964a2f797326207c
deepak7843 <deepakshankardubey123@gmail.com>
2022-07-20 20:19:56 +0530

    footer

--- travelocity/src/App.js
+++ travelocity/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Homepage from './Pages/Homepage';
+import { Footer } from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Homepage />
+      <Footer/>
     </div>
   );
 }
