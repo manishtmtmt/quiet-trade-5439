60a9f98a26c18805687c47ab72d6158db6f004b2
Sai-Narasimha <bukkapatnam.sainarasimha@gmail.com>
2022-07-22 16:55:54 +0530

    nav bar implemented

--- travelocity/src/Pages/Homepage.jsx
+++ travelocity/src/Pages/Homepage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Navbar } from '../Components/Navbar'
 import { LandingPage } from './LandingPage'
 
 const Homepage = () => {
   return (
     <div>
+      <Navbar/>
       <LandingPage/>
     </div>
   )
